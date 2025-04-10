2a5aef7249ee1e545fa611b8b0c557274b6a61ad
ly <mengqing20101212@163.com>
2025-04-10 21:29:43 +0800

    新增 protobuf相关工具类

---
+++ proto/proto_win.bat
@@ -0,0 +1,19 @@
+@echo off
+chcp 65001 >nul
+
+:: 设置输出目录
+set "outputDir=..\server\proto\src\main\java"
+
+:: 创建输出目录（如果不存在）
+if not exist "%outputDir%" (
+    mkdir "%outputDir%"
+)
+
+:: 编译所有 proto 文件到指定 Java 目录
+.\bin\protoc.exe --proto_path=. --java_out="%outputDir%" *.proto
+
+:: 可选：列出生成的文件
+echo Generated Java files:
+dir /s /b "%outputDir%"
+
+pause
