412ed945497b12371ed2b40f5173c0d80b8323e0
ly <mengqing20101212@163.com>
2025-04-11 20:03:03 +0800

    新增 loginServer 模块

--- server/LoginServer/src/main/java/ly/loginserver/LoginServerApplication.java
+++ server/LoginServer/src/main/java/ly/loginserver/LoginServerApplication.java
@@ -1,13 +1,26 @@
 package ly.loginserver;
 
+import javax.annotation.PostConstruct;
+import ly.ServerContext;
+import ly.config.ServerTypeEnum;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(LoginServerConfig.class)
 public class LoginServerApplication {
+  @Autowired private LoginServerConfig loginServerConfig;
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoginServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(LoginServerApplication.class, args);
+  }
 
+  @PostConstruct
+  public void init() {
+    System.out.println("nacosUrl:" + loginServerConfig.getNacosUrl());
+    ServerContext.startUp(
+        loginServerConfig.getNacosUrl(), ServerTypeEnum.LOGIN.getType(), "loginServer", "ly");
+  }
 }
